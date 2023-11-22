6ea83f2eda22a74ca0ed8817d1f82f5546b4d5e2
seralopez <sergio.alb.lopez@gmail.com>
2023-11-22 01:04:07 -0300

    Solucionado GetPrestadores

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const { Sequelize, Model, DataTypes, QueryTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 const { body, validationResult } = require('express-validator');
+const haversine = require('haversine-distance')
 app.use(express.json());
+var cors = require('cors')
+app.use(cors())
 // ==================================== VARIABLES ENTORNO =========================================
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
@@ -159,23 +162,47 @@ async function sql_update(tabla_nombre, columna_nombre, valor) {
     return resul
 }
 // ==================================== PRESTADOR * ===========================================++++
-app.post("/prestadores/", prestadores_ver, badRequest)
+app.get("/prestadores", prestadores_ver, badRequest)
 async function prestadores_ver(req, res, next) {
-    rol = req.body.rol
+    //rol = req.body.rol
     tabla_nombre = `tbl_usuarios JOIN tbl_servicios on tbl_servicios.servicios_usuario = tbl_usuarios.usuario_id`
+    tabla_nombre += ' JOIN tbl_datos on tbl_datos.datos_id = tbl_usuarios.usuario_id'
     columna_nombre = `tbl_servicios.servicios_nombre`
-    const check = sql_result(tabla_nombre, columna_nombre, `"${rol}"`)
-    //const resultado = await sequelize.query(`SELECT * from  WHERE tbl_servicios.servicios_nombre = "${rol}";`,
-    //  { type: QueryTypes.SELECT })
+    //const check = sql_result(tabla_nombre, columna_nombre, `"${rol}"`)
+    const check = sql_result(tabla_nombre, columna_nombre, `"Plomero"`)
     check.then(val => {
         if (val) {
-            /*columna_nombre = 'usuario_nombre,usuario_apellido,usuario_pass,usuario_correo,usuario_datos,usuario_rol'
-            datos_sql = `'${nombre}', '${apellido}', '${pass}', '${correo}', ${datos_usr}, ${rol}`
-            const sql_insert_resultado = sql_insert('tbl_usuarios', columna_nombre, datos_sql)
-            sql_insert_resultado.then(valor => {
-                res.json({ msg: `Usuario guardado con el ID : ${valor}` })
-            }) */
-            res.json({ msg: val })
+            const a = { latitude: -34.70207407721391, longitude: -58.371419282884325 }
+            let datos = []
+
+            val.forEach(element => {
+                const gps = element.datos_gps.split(',')
+                const b = { latitude: gps[0], longitude: gps[1] }
+                const distancia = ((haversine(a, b)) / 1000).toString().slice(0, 4)
+                datos.push({
+                    usuario_id: element.usuario_id,
+                    usuario_nombre: element.usuario_nombre,
+                    usuario_apellido: element.usuario_apellido,
+                    usuario_correo: element.usuario_correo,
+                    usuario_ultConec: element.usuario_ultConec,
+                    usuario_foto: element.usuario_foto,
+                    usuario_rol: element.usuario_rol,
+                    servicios_usuario: element.servicios_usuario,
+                    servicios_nombre: element.servicios_nombre,
+                    servicios_matricula: element.servicios_matricula,
+                    datos_id: element.datos_id,
+                    datos_telefono: element.datos_telefono,
+                    datos_pais: element.datos_pais,
+                    datos_provincia: element.datos_provincia,
+                    datos_localidad: element.datos_localidad,
+                    datos_calle: element.datos_calle,
+                    datos_gps: distancia,
+                    datos_disponibilidad: element.datos_disponibilidad,
+                    datos_servicios: element.datos_servicios,
+                    datos_calificacion: element.datos_calificacion
+                })
+            });
+            res.json(datos)
         } else {
             next(`No se encontro prestador.`)
         }
